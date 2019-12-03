793cc912846585dbc4da4c0fe829511c1a2b893d
Renato Novais <renatonovais@gmail.com>
2019-12-02 22:39:15 -0300

    versão do CRUD em memória

--- example/controle_ocorrecia_dart_example.dart
+++ example/controle_ocorrecia_dart_example.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:controle_ocorrecia_dart/controle_ocorrecia_dart.dart';
 
 int menu() {
+  //limparTela();
   print('### Controle de Ocorrências ###');
   print('Digite uma opção');
   print('0. Sair');
@@ -14,8 +15,62 @@ int menu() {
   return int.parse(val);
 }
 
+void inserirOcorrencia(List<Ocorrencia> lista) {
+  print('Digite a ocorrência');
+  String description = stdin.readLineSync();
+  Ocorrencia oco = Ocorrencia(description);
+  lista.add(oco);
+  print('Item inserido com sucesso');
+}
+
+void excluirOcorrencia(List<Ocorrencia> lista) {
+  print('Digite o id da Ocorrência');
+  int id = int.parse(stdin.readLineSync());
+  bool deletado = false;
+  for (var oco in lista) {
+    if (oco.id == id) {
+      lista.remove(oco);
+      deletado = true;
+      break;
+    }
+  }
+  if (deletado) {
+    print('Item deletado com sucesso');
+  } else {
+    print('Ocorrência não encontrada');
+  }
+}
+
+void atualizarOcorrencia(List<Ocorrencia> lista) {
+  print('Digite o id da Ocorrência');
+  int id = int.parse(stdin.readLineSync());
+  bool achou = false;
+
+  for (var oco in lista) {
+    if (oco.id == id) {
+      print('Digite a nova descrição da Ocorrência');
+      String description = stdin.readLineSync();
+      oco.description = description;
+      achou = true;
+      break;
+    }
+  }
+  if (achou) {
+    print('Item atualizado com sucesso');
+  } else {
+    print('Ocorrência não encontrada');
+  }
+}
+
+void limparTela() {
+  for (int i = 0; i < stdout.terminalLines; i++) {
+    stdout.writeln();
+  }
+}
+
 main() {
   int sair;
+  List<Ocorrencia> listaOcorrencias = List();
   do {
     sair = menu();
     switch (sair) {
@@ -24,17 +79,20 @@ main() {
         break;
       case 1:
         print('inserindo....');
+        inserirOcorrencia(listaOcorrencias);
         break;
       case 2:
         print('excluindo....');
+        excluirOcorrencia(listaOcorrencias);
         break;
       case 3:
         print('atualizando....');
+        atualizarOcorrencia(listaOcorrencias);
         break;
       case 4:
         print('listando....');
+        print(listaOcorrencias);
         break;
-
       default:
         print('opção inválida....');
     }
--- lib/src/controle_ocorrecia_dart_base.dart
+++ lib/src/controle_ocorrecia_dart_base.dart
@@ -1,6 +1,41 @@
 // TODO: Put public facing types in this file.
 
+import 'package:controle_ocorrecia_dart/controle_ocorrecia_dart.dart';
+
 /// Checks if you are awesome. Spoiler: you are.
 class Awesome {
   bool get isAwesome => true;
+}
+
+//classe que representa uma ocorrência
+class Ocorrencia {
+  static int idCount = 0;
+  int id;
+  String description;
+  TipoOcorrencia tipoOcorrencia;
+
+  Ocorrencia(String description) {
+    idCount++;
+    id = idCount;
+    this.description = description;
+  }
+
+  @override
+  String toString() {
+    return id.toString() + ' - ' + description;
+  }
+
+  // Ocorrencia.fromJson(Map<String, dynamic> json)
+  //     : id = json['id'],
+  //       description = json['description'];
+
+  // Map<String, dynamic> toJson() => {
+  //       'id': id,
+  //       'description': description,
+  //     };
+}
+
+class TipoOcorrencia {
+  int id;
+  String description;
 }
